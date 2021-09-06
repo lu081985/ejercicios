9dc28d87d380ce3dbf084af3732cef4aa71b383a
lu081985 <perez.guadalupe1807@gmail.com>
2021-09-06 15:32:10 -0500

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h> //standar input output
+/*
+Descripción: Uso de variables
+Autor: me
+date: 6-9-2021
+*/
+
+int main(void){
+//pudemos usar "void" en lugar de "int" sin problema
+
+// tipoDato identificador=valor inicial;
+int mishi=0; //declarando entero e iniciandolo en variable 
+const int lomito=0; //Declarando constante entera e iniciandola en 0
+
+//printf("Etiqueta formatos", variable);
+printf("Mi variable mishi tiene el valor de %i", mishi);
+printf ("\nMi variable lomito tiene el valor de %i", lomito); 
+
+mishi=100;
+//lomito= 200;
+
+printf("\nMi variable mishi tiene el valor de %i", mishi);
+printf ("\nMi variable lomito tiene el valor de %i", lomito);
+//TODO
+
+}
